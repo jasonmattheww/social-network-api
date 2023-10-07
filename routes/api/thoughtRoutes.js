57366cc70c0f42b1ba5bc4cd006842f2911b4ce2
Jason <jasontorrealba199@gmail.com>
2023-10-07 10:02:56 -0400

    Fixed route error

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -14,7 +14,7 @@ router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router
-  .route('/:thoughtsId')
+  .route('/:thoughtId')
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
